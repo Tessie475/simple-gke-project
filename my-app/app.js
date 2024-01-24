62fdb1430f0fee9450e893a96066fa12eacff54e
Tessie475 <eberechichukwu1@gmail.com>
2024-01-24 17:44:20 +0100

    added a simple javascript file

---
+++ my-app/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Code to execute after the DOM is fully loaded
+    console.log('DOM is ready.');
+
+    // Select the #app element and update its content
+    var appElement = document.getElementById('app');
+    appElement.innerHTML = '<h1>Hello, World!</h1>';
+});
